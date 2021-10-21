5cadcd0a10090dfd23f5b924ef13a234ecde4b3f
orionsolex <orionsolex@gmail.com>
2021-10-21 20:32:23 +0300

    uncomment

--- bot.py
+++ bot.py
@@ -20,32 +20,32 @@ dp.filters_factory.bind(IsAdminFilter)
 async def echo(message: types.Message):
     await message.answer(message.text)
 
-#
-# # remove new user joined messages
-# @dp.message_handler(content_types=["new_chat_members"])
-# async def on_user_joined(message: types.Message):
-#     await message.delete()
-#
-#
-# # ban command (admins only)
-# @dp.message_handlers(is_admin=True, commands=["ban"], commands_prefix="!/")
-# async def cmd_ban(message: types.Message):
-#     if not message.reply_to_message:
-#         await message.reply("Нецензурная лексика запрещена правилами Группы")
-#         return
-#     await message.bot.delete_message(chat_id=config.GROUP_ID, message_id=message.message_id)
-#     await message.bot.kick_chat_member(chat_id=config.GROUP_ID, user_id=message.reply_to_message)
-#     await message.reply_to_message.reply("Пользователь забанен!")
-#
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#
-# # Press the green button in the gutter to run the script.
-# # if __name__ == '__main__':
-# #   print_hi('PyCharm')
-#
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# remove new user joined messages
+@dp.message_handler(content_types=["new_chat_members"])
+async def on_user_joined(message: types.Message):
+    await message.delete()
+
+
+# ban command (admins only)
+@dp.message_handlers(is_admin=True, commands=["ban"], commands_prefix="!/")
+async def cmd_ban(message: types.Message):
+    if not message.reply_to_message:
+        await message.reply("Нецензурная лексика запрещена правилами Группы")
+        return
+    await message.bot.delete_message(chat_id=config.GROUP_ID, message_id=message.message_id)
+    await message.bot.kick_chat_member(chat_id=config.GROUP_ID, user_id=message.reply_to_message)
+    await message.reply_to_message.reply("Пользователь забанен!")
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#   print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 # run long_polling
 if __name__ == "__main__":
